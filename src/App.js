8c58b679e00b411f898fc48630ca63addf365ee4
Егор <egornesvickij@gmail.com>
2021-07-06 23:46:24 +0500

    added examples of usage the material-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+// импортируем элементы Material-UI
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+// краткая запись: import {makeStyles, Button} from '@material-ui/core/';
+
+import { classes } from 'istanbul-lib-coverage';
 
 function App() {
+
+  // Для применения стилей к material элементам
+  const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  }, testButtons: {
+        '&': {
+          margin: 40,
+        }
+      },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Вставляем импортированный элемент */}
+      <Button variant="contained" color="primary" disableElevation className={classes.testButtons}>
+        Пример кнопки Material-UI
+      </Button>
+      <Button variant="contained" disabled className={classes.testButtons}>
+        Пример №2 'disabled'
+      </Button>
     </div>
   );
 }
