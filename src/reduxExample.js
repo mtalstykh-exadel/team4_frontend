aee7f27210735aad4409c070ce91623fe3872edb
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-07-07 00:23:46 +0300

    install redux

---
+++ src/reduxExample.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./App";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(counter, composeEnhancers(applyMiddleware(thunk)));
+
+function counter(state = 0, action) {
+    return state;
+  }
+
+function Apps() {
+    return (
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    );
+}
+
+  export default Apps;
