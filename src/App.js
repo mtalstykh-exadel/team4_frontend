cb49457abc77f41e56b1d67db975064183b22249
User <toxa.saidoff@gmail.com>
2021-07-06 19:59:59 +0500

    init react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/login'>Login</Link>
+      <Switch>
+        <Route path='/login' render={() => <Login/>}/>
+      </Switch>
     </div>
   );
 }
+//Позже будет вынесен в отдельный компонент 
+const Login = () => {
+  return <div>
+    Login
+  </div>
+}
 
-export default App;
+const AppContainer = () => {
+  return <BrowserRouter>
+    <App />
+  </BrowserRouter>
+}
+
+export default AppContainer;
