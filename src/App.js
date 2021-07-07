d1608dc459593f42784cddcc8a2f6eaaeb229592
User <toxa.saidoff@gmail.com>
2021-07-07 15:54:57 +0500

    init react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//importing login page
+import Login from './Components/Login/Login';
+//importing components from react-router-dom for routing
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // importing the elements Material-UI
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
@@ -10,15 +13,15 @@ function App() {
 
   // To apply styles to material elements
   const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  }, testButtons: {
-        '&': {
-          margin: 40,
-        }
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
       },
+    }, testButtons: {
+      '&': {
+        margin: 40,
+      }
+    },
   }));
 
   const classes = useStyles();
@@ -26,14 +29,29 @@ function App() {
   return (
     <div className="App">
       {/* Adding the imported element */}
+      {/* Link to the path login */}
+      <Button variant="contained" color="primary" disableElevation className={classes.testButtons}>
+        
+        <Link to='/login'>Login</Link>
+      </Button>
       <Button variant="contained" color="primary" disableElevation className={classes.testButtons}>
         Example button Material-UI
       </Button>
       <Button variant="contained" disabled className={classes.testButtons}>
         Example №2 'disabled'
       </Button>
+      {/* adding routing */}
+      <Switch>
+        <Route path='/login' render={() => <Login />} />
+      </Switch>
     </div>
   );
 }
+// wrapping with container component for routing
+const AppContainer = () => {
+  return <BrowserRouter>
+    <App />
+  </BrowserRouter>
+}
 
-export default App;
+export default AppContainer;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginForm from './LoginForm/LoginForm'
+
+const Login = () => {
+    return <div>
+        {/* form of logging in */}
+        <LoginForm />
+    </div>
+}
+
+export default Login
