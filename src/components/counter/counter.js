e074b06b71f3023cdec3296f236ea334345416c6
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-07-13 15:26:54 +0300

    strict rules for eslint

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -6,37 +6,37 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
 
 function Counter() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
-    }, testButtons: {
-      '&': {
-        margin: 40,
-      }
-    },
-  }));
-  const classes = useStyles();
+	const useStyles = makeStyles((theme) => ({
+		root: {
+			'& > *': {
+				margin: theme.spacing(1),
+			},
+		}, testButtons: {
+			'&': {
+				margin: 40,
+			}
+		},
+	}));
+	const classes = useStyles();
 
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
+	const counter = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
 
-  return (
-    <div className="App">
-      <div>{counter}</div>
-      <div>
-        <button onClick={() => { dispatch(increment()); }}>INCREMENT BY 1</button>
-      </div>
-      <div>
-        <button onClick={() => { dispatch(decrement()); }}>DECREMENT BY 1</button>
-      </div>
-      <button onClick={() => { dispatch(reset()); }}>RESET</button>
-      <Button variant="contained" color="primary" disableElevation className={classes.testButtons}>
-        <Link to='/profile'>Profile</Link>
-      </Button>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>{counter}</div>
+			<div>
+				<button onClick={() => { dispatch(increment()); }}>INCREMENT BY 1</button>
+			</div>
+			<div>
+				<button onClick={() => { dispatch(decrement()); }}>DECREMENT BY 1</button>
+			</div>
+			<button onClick={() => { dispatch(reset()); }}>RESET</button>
+			<Button variant="contained" color="primary" disableElevation className={classes.testButtons}>
+				<Link to='/profile'>Profile</Link>
+			</Button>
+		</div>
+	);
 }
 
 export default Counter;
