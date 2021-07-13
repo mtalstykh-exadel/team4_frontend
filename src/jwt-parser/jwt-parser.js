cdfac8f1895270027d1090f5379af3d94cd489f6
DmitryqV <vlasenko_dima@icloud.com>
2021-07-13 13:18:17 +0500

    jwt decode

--- src/jwt-parser/jwt-parser.js
+++ src/jwt-parser/jwt-parser.js
@@ -1,16 +1,12 @@
+import jwt_decode from "jwt-decode";
+
 class JWT{
     constructor(){
         this.jwtKey = "jwt=data";
         this.data;
     }
     parse(token){
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c){
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
-
-        this.data = JSON.parse(jsonPayload);
+        this.data = jwt_decode(token);
         this.save();
     }
     save(){
