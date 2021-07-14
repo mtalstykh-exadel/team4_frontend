e48a70730a34d8d883232238008226c2dc64a3a2
Alesia Drazdova <76945498+alesiadrazdova@users.noreply.github.com>
2021-07-14 10:32:03 +0300

    Update main.jsx

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div>
+        <>
             <h2>Testing procedure</h2>
             <p>Choose a test according to the expected level of English. If you don't know your level, then start with the first test.</p>
-        </div>
+        </>
     );
 };
 
