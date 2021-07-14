a87442999b90f5df95f6cea964093f5a4c5894ea
DmitryqV <vlasenko_dima@icloud.com>
2021-07-14 15:52:25 +0500

    fix spaces

--- src/utils/jwt-parser.js
+++ src/utils/jwt-parser.js
@@ -1,23 +1,23 @@
 import jwt_decode from "jwt-decode";
 
-class JWT{
-    constructor(){
-        this.jwtKey = "jwt=data";
-        this.data;
-    }
-    parse(token){
-        this.data = jwt_decode(token);
-        this.save();
-    }
-    save(){
-        localStorage.setItem(this.jwtKey, JSON.stringify(this.data));
-    }
-    get(){
-        return JSON.parse(localStorage.getItem(this.jwtKey));
-    }
-    remove(){
-        localStorage.removeItem(this.jwtKey);
-    }
+class JWT {
+  constructor() {
+    this.jwtKey = "jwt=data";
+    this.data;
+  }
+  parse(token) {
+    this.data = jwt_decode(token);
+    this.save();
+  }
+  save() {
+    localStorage.setItem(this.jwtKey, JSON.stringify(this.data));
+  }
+  get() {
+    return JSON.parse(localStorage.getItem(this.jwtKey));
+  }
+  remove() {
+    localStorage.removeItem(this.jwtKey);
+  }
 }
 
 export default new JWT();
