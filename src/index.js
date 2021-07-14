183fc39465615733d2ad16bd0c1759d776e41d4a
ИТ-201 <it-201@comp12.741studios.ru>
2021-07-14 16:15:28 +0500

    mini fix

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
-//Start on eng
+// Start on eng
 import catalogEn from './locales/en/messages.js';
 i18n.load('en', catalogEn.messages);
 i18n.activate('en');
 
 ReactDOM.render(
-  <I18nProvider i18n={i18n}>
-    <App />
-  </I18nProvider>,
+  <React.StrictMode>
+    <I18nProvider i18n={i18n}>
+      <App />
+    </I18nProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 reportWebVitals();
--- src/locales/ru/messages.js
+++ src/locales/ru/messages.js
@@ -1,1 +1,7 @@
-
+/*eslint-disable*/
+module.exports = {
+    messages: {
+        "Example button Material-UI": "Пример кнопки Material-UI",
+        "Example №2 disabled": "Пример №2 disabled"
+    }
+};
