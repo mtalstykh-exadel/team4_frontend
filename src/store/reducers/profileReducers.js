f257661b879f616c93f142929946a3037032c352
User <toxa.saidoff@gmail.com>
2021-07-14 20:38:03 +0500

    init got data from develop branch

--- src/store/reducers/profileReducers.js
+++ src/store/reducers/profileReducers.js
@@ -1,24 +1,24 @@
 import { SET_PROFILE_DATA } from "../actions/actionTypes";
 
 const initialState = {
-	avatar: null,
-	fullname: null,
-	occupation: null,
-	email: null
+  avatar: null,
+  fullname: null,
+  occupation: null,
+  email: null
 };
 
 const profileReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case SET_PROFILE_DATA:
-			return Object.assign({}, state, {
-				avatar: action.avatar,
-				fullname: action.fullname,
-				occupation: action.occupation,
-				email: action.email
-			});
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case SET_PROFILE_DATA:
+      return Object.assign({}, state, {
+        avatar: action.avatar,
+        fullname: action.fullname,
+        occupation: action.occupation,
+        email: action.email
+      });
+    default:
+      return state;
+  }
 };
 
 export { profileReducer };
