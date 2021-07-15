24e33dcc6ff32236480b089fa90c4d26583db737
DmitryqV <vlasenko_dima@icloud,com>
2021-07-15 21:51:41 +0800

    fix style code

--- src/index.js
+++ src/index.js
@@ -5,16 +5,14 @@ import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
-// Start on eng
-import catalogEn from '@lingui/loader!./locales/en/messages.po';
-import catalogRu from '@lingui/loader!./locales/ru/messages.po';
-console.log(catalogEn, catalogRu);
-i18n.load('en', catalogEn.messages);
-i18n.load('ru', catalogRu.messages);
-i18n.activate('en');
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import catalogEn from '@lingui/loader!./locales/en/messages.po';
+import catalogRu from '@lingui/loader!./locales/ru/messages.po';
+i18n.load('en', catalogEn.messages);
+i18n.load('ru', catalogRu.messages);
+i18n.activate('en');
 
 const AppContainer = () => {
   return <BrowserRouter>
