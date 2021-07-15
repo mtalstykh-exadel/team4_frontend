d244d0af45a45cf5e6cfccaa5ad1e5a6a0005624
DmitryqV <vlasenko_dima@icloud.com>
2021-07-15 17:41:44 +0500

    fix gitignore file

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { i18n } from '@lingui/core';
 import { I18nProvider } from '@lingui/react';
 // Start on eng
-import catalogEn from './locales/en/messages.js';
-import catalogRu from './locales/ru/messages.js';
-
+import catalogEn from '@lingui/loader!./locales/en/messages.po';
+import catalogRu from '@lingui/loader!./locales/ru/messages.po';
+console.log(catalogEn, catalogRu);
 i18n.load('en', catalogEn.messages);
 i18n.load('ru', catalogRu.messages);
 i18n.activate('en');
--- src/locales/ru/messages.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable*/
-module.exports = {
-    messages: {
-        "Example button Material-UI":"ПРИМЕР КНОПКИ Material-UI",
-        "Example №2 disabled":"ПРИМЕР №2 ОТКЛЮЧЕН"
-    }
-};
