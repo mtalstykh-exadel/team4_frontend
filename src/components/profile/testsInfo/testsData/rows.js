4b7cd2e1b392fa7695fbcb055ee643766ccf4dfe
User <toxa.saidoff@gmail.com>
2021-07-16 18:18:50 +0500

    init solved issues

--- src/components/profile/testsInfo/testsData/rows.js
+++ src/components/profile/testsInfo/testsData/rows.js
@@ -1,17 +1,18 @@
 const rows = [
   {
-    level: 'A1', 
-    assigned: '19 Jun 2021, 10:52', 
-    deadline: '24 Jul 2021, 10:53', 
-    dateVerified: '24 Jul 2021, 10:54', 
-    status: 'assigned', 
-    result: 'not passed', 
-    action: 'Take test', 
-    id: 1},
+    level: 'A1',
+    assigned: '19 Jun 2021, 10:52',
+    deadline: '24 Jul 2021, 10:53',
+    dateVerified: '24 Jul 2021, 10:54',
+    status: 'assigned',
+    result: 'not passed',
+    action: 'Take test',
+    id: 1
+  },
 ];
 
 for (let i = 2; i < 203; i++) {
-  rows.push({...rows[0], id: i});
+  rows.push({ ...rows[0], id: i });
 }
 
-export {rows};
+export { rows };
