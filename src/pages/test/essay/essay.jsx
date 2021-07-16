173f18765b1145d761b57f384d4dfbf4d2a9ba25
petemarianne <petia2001@icloud.com>
2021-07-16 17:32:43 +0300

    essay page

--- src/pages/test/essay/essay.jsx
+++ src/pages/test/essay/essay.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
+import './essay.scss';
 
-const Essay = () => {
-  return <div className='stepDescription'>Write an essay on a given topic</div>;
-};
+export default class Essay extends Component {
+  state = {
+    value: '',
+    length: 0
+  };
 
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
 
-export default Essay;
+  getValueLength = () => {
+    this.setState({length: this.state.value.length});
+  }
+
+  render() {
+    return (
+      <>
+        <div className='stepDescription'>Write an essay on a given topic</div>
+        <div className='essayTopic'>Essay Topic</div>
+        <div className='essayInputArea'>
+          <textarea onPaste={(event) => {event.preventDefault(); return false;}}
+                    onCopy={(event) => {event.preventDefault(); return false;}}
+                    onCut={(event) => {event.preventDefault(); return false;}}
+                    className='essayInput' maxLength='512' value={this.state.value}
+                    onChange={this.handleChange} onKeyUp={this.getValueLength} onKeyDown={this.getValueLength}/>
+        </div>
+        <div className='essayCount'>{this.state.length} out of 512 characters</div>
+      </>
+    );
+  }
+}
