4f14acfa53b077bc7ae62541343613da8ce89df3
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-07-17 17:04:49 +0300

    update of header reducers

--- src/store/reducers/headerReducers.js
+++ src/store/reducers/headerReducers.js
@@ -1,15 +1,15 @@
 import { LANGUAGE_RUSSIAN, LANGUAGE_ENGLISH } from '../actions/actionTypes';
 import { USER_DEFAULT, USER_ADMIN, USER_HR, USER_COACH } from '../actions/actionTypes';
 
-const languageReducer = (state = 'english', action) => {
+const languageReducer = (state = {type: 'english', short: 'EN'}, action) => {
   switch (action.type) {
-    case LANGUAGE_RUSSIAN: return Object.assign({}, state, {language: 'russian'});
-    case LANGUAGE_ENGLISH: return Object.assign({}, state, {language: 'english'});
+    case LANGUAGE_RUSSIAN: return Object.assign({}, state, {type: 'russian', short: 'RU'});
+    case LANGUAGE_ENGLISH: return Object.assign({}, state, {type: 'english', short: 'EN'});
     default: return state;
   }
 };
 
-const roleReducer = (state = 'default', action) => {
+const roleReducer = (state = 'admin', action) => {
   switch (action.type) {
     case USER_DEFAULT: return 'default';
     case USER_HR: return 'hr';
