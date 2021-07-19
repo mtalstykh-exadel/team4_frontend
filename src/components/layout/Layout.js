4292f8406bd639b24638fb279df531a9092e5e0c
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-07-19 20:19:12 +0300

    fix layout mistake

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -7,7 +7,7 @@ import Header from '../../components/header/header';
 const Layout = ({children}) => {
   return (
     <>
-      {<Header/>}
+      <Header/>
       <main className="main">
         {children}
       </main>
