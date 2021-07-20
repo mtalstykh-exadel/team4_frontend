82832a4bf84bd7b8816419b9dbbb7240078e6776
Kseniya <64595063+pupkoksenia@users.noreply.github.com>
2021-07-20 14:42:29 +0300

    Update Modal.js

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,26 +1,7 @@
 import React from "react";
-// import "./Modal.scss";
-/* import PropTypes from 'prop-types';
-
-const Modal = ({ active, setActive, children }) => {
-    return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
-            <div className={active ? "modal__content active" : "modal__content"} onClick={e => e.stopPropagation}>
-                {children}
-            </div>
-        </div>
-    );
-};
-
-Modal.propTypes = {
-    active: PropTypes.bool,
-    setActive: PropTypes.func,
-    children: PropTypes.string
-};
-export default Modal;
-*/
 import Modal from '@material-ui/core/Modal';
-import {makeStyles} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
 function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
