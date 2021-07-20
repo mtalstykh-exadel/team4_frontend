5e31415522f0969e5d1c49683c272145fe39b41a
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-07-20 13:21:44 +0300

    changes made

--- src/components/EditTestsSelector/EditTestsSelector.jsx
+++ src/components/EditTestsSelector/EditTestsSelector.jsx
@@ -3,13 +3,10 @@ import EditTestsInfo from './EeditTestsInfo/EditTestsInfo';
 import imageSrc from '../../assets/images/goose.svg';
 import './EditTestsSelector.scss';
 
-
 const EditTestsSelector = () => {
   return (
     <div className='edit-tests-selector'>
-      <div>
-        <EditTestsInfo />
-      </div>
+      <EditTestsInfo />
       <div className='edit-tests-placeholder'>
         <div className='edit-tests-selector-text'>Select the option at the top to see the questions.</div>
         <img title='goose-img' alt='goose-img' src={imageSrc} className='picture' />
