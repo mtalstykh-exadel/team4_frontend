ee359ae67a37be4e8aee77309b3670df93f6450a
petemarianne <petia2001@icloud.com>
2021-07-21 20:15:05 +0300

    go home button fix

--- src/pages/Page404/Page404.js
+++ src/pages/Page404/Page404.js
@@ -1,25 +1,25 @@
 import React from 'react';
-
 import './Page404.scss';
 import logo from '../../assets/images/logo/logo.svg';
 import Button from '@material-ui/core/Button';
+import {Link} from "react-router-dom";
 
 const Page404 = () => {
   return (
-    <div className='page404'>
-      <div className='logo404-wrapper'>
-        <span>4</span>
-        <span><img src={logo} alt='logo' className='logo'/></span>
-        <span>4</span>
+      <div className='page404'>
+        <div className='logo404-wrapper'>
+          <span>4</span>
+          <span><img src={logo} alt='logo' className='logo'/></span>
+          <span>4</span>
+        </div>
+        <div className='message-wrapper'>
+          <h1>Oops, this page was not found!</h1>
+          <h3>Either something went wrong or the page doesn't exist anymore.</h3>
+        </div>
+        <div className='button-wrapper'>
+          <Button color='primary' variant='contained' component={Link} to='/'>Go Home</Button>
+        </div>
       </div>
-      <div className='message-wrapper'>
-        <h1>Oops, this page was not found!</h1>
-        <h3>Either something went wrong or the page doesn't exist anymore.</h3>
-      </div>
-      <div className='button-wrapper'>
-        <Button color='primary' variant='contained'>Go Home</Button>
-      </div>
-    </div>
   );
 };
 
