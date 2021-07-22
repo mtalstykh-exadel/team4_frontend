d3304ce007af90346c43b2a0837f73dab2ab5658
User <toxa.saidoff@gmail.com>
2021-07-22 16:03:54 +0500

    realised autoSubmit

--- src/components/testsInfo/TestsInfoSearchForm/TestsInfoSearchForm.jsx
+++ src/components/testsInfo/TestsInfoSearchForm/TestsInfoSearchForm.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
-import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
@@ -15,20 +14,23 @@ const TestInfoSearchForm = (props) => {
   };
 
   const formik = useFormik({ initialValues: { level: '' }, validationSchema: null, onSubmit });
+
+  useEffect(() => {
+    formik.submitForm();
+  }, [formik.values]);
   return (
     <div className='searchForm'>
       <form onSubmit={formik.handleSubmit}>
         <FormControl variant="outlined" className='formControl' size='small'>
           <InputLabel htmlFor="level">Level</InputLabel>
-          <Select native value={formik.values.level} name='level' onChange={formik.handleChange} label="Level"
-            inputProps={{ name: 'level' }}>
+          <Select native value={formik.values.level} name='level' onChange={formik.handleChange}
+            label="Level"
+            inputProps={{ name: 'level' }}
+          >
             <option aria-label="All" value={null} />
             {testLevels.map((el) => <option key={testLevels.indexOf(el)}>{el}</option>)}
           </Select>
         </FormControl>
-        <Button color="primary" variant="contained" type="submit" className='submitButton'>
-          Submit
-        </Button>
       </form>
     </div>
   );
