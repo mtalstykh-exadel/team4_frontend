362d3b304bc7c53bcb2c1bcafbed299a60b63524
Kseniya <64595063+pupkoksenia@users.noreply.github.com>
2021-07-23 15:16:52 +0300

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,1 +1,23 @@
+import "./Layout.scss";
+import React from "react";
 
+import { PropTypes } from "prop-types";
+import Header from '../../components/header/header';
+
+const Layout = ({ pageWrapperClass, children }) => {
+  return (
+    <>
+      <Header/>
+      <main className={pageWrapperClass}>
+        {children}
+      </main>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.array,
+  pageWrapperClass: PropTypes.string,
+};
+
+export default Layout;
