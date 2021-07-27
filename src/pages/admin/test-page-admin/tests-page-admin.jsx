235615a1c52e58c46f14ce502ed6e987a8b8741d
ИТ-201 <egornesvickij@gmail.com>
2021-07-27 15:21:35 +0500

    moved style for select in scss

--- src/pages/admin/test-page-admin/tests-page-admin.jsx
+++ src/pages/admin/test-page-admin/tests-page-admin.jsx
@@ -23,17 +23,17 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const SelectItem = withStyles(() => ({
-  root: {
-    minWidth: 345,
-    minHeight: 30,
-    boxSizing: "border-box",
-    padding: 0,
-    paddingBottom: "auto",
-    paddingTop: "auto",
-    paddingLeft: 21,
-  }
-}))(Select);
+//const SelectItem = withStyles(() => ({
+//  root: {
+//    minWidth: 345,
+//    minHeight: 30,
+//    boxSizing: "border-box",
+//    padding: 0,
+//    paddingBottom: "auto",
+//    paddingTop: "auto",
+//    paddingLeft: 21,
+//  }
+//}))(Select);
 
 const ButtonAssign = withStyles(() => ({
   root: {
@@ -76,99 +76,102 @@ const AdminTestPage = (props) => {
   const classes = useStyles();
 
   return (
-    <Layout>
-      <Paper>
-        <TableContainer className="paper">
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {columns.map((column) => (
-                  <TableCell
-                    className={classes.headItems}
-                    size="small"
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                  >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredRows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                      {columns.map((column) => {
-                        const value = row[column.id];
-                        keysForColumns++;
-                        return (
-                          <TableCell
-                            key={keysForColumns}
-                            align={column.align}
-                            size="small"
-                            classes={{
-                              rootCell: classes.rootCell
-                            }}
-                          >
-                            {column.id === "Coach" ? (
-                              <SelectItem
-                                native
-                                variant="outlined"
-                                defaultValue="placeholder"
-                                color="red"
-                                classes={{
-                                  root: classes.root,
-                                }}
-                              >
-                                <option aria-label="None" value="placeholder">name</option>
-                                {coaches.map((coachName) => {
-                                  keysForOptions++;
-                                  return (
-                                    <option key={keysForOptions} value={coachName}>
-                                      {coachName}
-                                    </option>
-                                  );
-                                })}
-                              </SelectItem>
-                            ) : (
-                              console.log('false')
-                            )}
+    <div className="AdminTestPage">
+      <Layout>
+        <Paper>
+          <TableContainer className="paper">
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {columns.map((column) => (
+                    <TableCell
+                      className={classes.headItems}
+                      size="small"
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredRows
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                        {columns.map((column) => {
+                          const value = row[column.id];
+                          keysForColumns++;
+                          return (
+                            <TableCell
+                              key={keysForColumns}
+                              align={column.align}
+                              size="small"
+                              classes={{
+                                rootCell: classes.rootCell
+                              }}
+                            >
+                              {column.id === "Coach" ? (
+                                <Select
+                                  className="selectCoachNames"
+                                  native
+                                  variant="outlined"
+                                  defaultValue="placeholder"
+                                  color="red"
+                                  classes={{
+                                    root: classes.root,
+                                  }}
+                                >
+                                  <option aria-label="None" value="placeholder">name</option>
+                                  {coaches.map((coachName) => {
+                                    keysForOptions++;
+                                    return (
+                                      <option key={keysForOptions} value={coachName}>
+                                        {coachName}
+                                      </option>
+                                    );
+                                  })}
+                                </Select>
+                              ) : (
+                                console.log('false')
+                              )}
 
-                            {column.id === "action" ? (
-                              <ButtonAssign
-                                variant="outlined"
-                                color="primary"
-                                size="small"
-                                onClick={() => testAction(row[column.id])}
-                              >
-                                {value}
-                              </ButtonAssign>
-                            ) : (
-                              value
-                            )}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[10]}
-          component="div"
-          count={filteredRows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-    </Layout >
+                              {column.id === "action" ? (
+                                <ButtonAssign
+                                  variant="outlined"
+                                  color="primary"
+                                  size="small"
+                                  onClick={() => testAction(row[column.id])}
+                                >
+                                  {value}
+                                </ButtonAssign>
+                              ) : (
+                                value
+                              )}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10]}
+            component="div"
+            count={filteredRows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Layout >
+    </div>
   );
 };
 
