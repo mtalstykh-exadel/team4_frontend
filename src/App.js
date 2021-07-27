21f93a02f9039ff18a1705787459e88dd20cf82b
Егор <egornesvickij@gmail.com>
2021-07-27 21:29:24 +0500

    fix errors

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Counter from './components/counter/counter';
 import PrivateRoute from './utils/privateRoute';
 import Profile from './pages/profile/profile';
 import Main from './pages/main/main';
-import DemoJWT from './pages/jwt/jwt.jsx';
-import AdminDistribution from './pages/adminDistribution/adminDistribution.jsx';
 import Page404 from "./pages/Page404/Page404";
 import EditTests from './pages/editTests/editTests';
 import '../src/styles/modal.scss';
@@ -27,8 +25,6 @@ const App = () => {
         <PrivateRoute path="/counter"><Counter /></PrivateRoute>
         <PrivateRoute path="/profile"><Profile /></PrivateRoute>
         <PrivateRoute path="/" exact><Main /></PrivateRoute>
-        <PrivateRoute path="/jwt" ><DemoJWT /></PrivateRoute>
-        <PrivateRoute path="/admin-distribution" ><AdminDistribution /></PrivateRoute>
         <PrivateRoute path="/edittests" ><EditTests /></PrivateRoute>
         <Route path="*" render={() => <Page404 />} />
       </Switch>
