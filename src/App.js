0802a8cc9170fbc8d99b3ccc2023794dbe9d7dda
Kseniya <64595063+pupkoksenia@users.noreply.github.com>
2021-07-27 11:36:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Login from './pages/login/login';
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
-import Buttons from './components/buttons/Buttons';
 import Counter from './components/counter/counter';
 import PrivateRoute from './utils/privateRoute';
 import Profile from './pages/profile/profile';
@@ -10,7 +9,7 @@ import Main from './pages/main/main';
 import DemoJWT from './pages/jwt/jwt.jsx';
 import Page404 from "./pages/Page404/Page404";
 import EditTests from './pages/editTests/editTests';
-import '../../styles/modal.scss';
+import '../src/styles/modal.scss';
 
 const App = () => {
 
@@ -18,7 +17,6 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route path='/login' render={() => <Login />} />
-        <PrivateRoute path="/buttons"><Buttons /></PrivateRoute>
         <PrivateRoute path="/counter"><Counter /></PrivateRoute>
         <PrivateRoute path="/profile"><Profile /></PrivateRoute>
         <PrivateRoute path="/" exact><Main /></PrivateRoute>
