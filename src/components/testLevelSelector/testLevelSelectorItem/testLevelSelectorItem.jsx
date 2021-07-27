ad1f8593c8f5f6d0f233ba6709dcb155ca87c039
DmitryqV <vlasenko_dima@icloud.com>
2021-07-27 19:23:43 +0500

    rename folder

--- src/components/testLevelSelector/testLevelSelectorItem/testLevelSelectorItem.jsx
+++ src/components/testLevelSelector/testLevelSelectorItem/testLevelSelectorItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from "@material-ui/core/Button";
 import { Link } from 'react-router-dom';
-import './testLevelSelectorItem.scss';
+import './itemStyle.scss';
 
 const TestLevelsSelectorItem = ({ title, description }) => {
 
