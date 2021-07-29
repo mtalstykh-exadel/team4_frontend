b01ac966b2877a7e431fb1cd8a8c5cfe02f07dda
DmitryqV <vlasenko_dima@icloud.com>
2021-07-29 12:15:32 +0500

    leaved it tests-for-verification

--- src/components/header/userNavigation/userNavigation.js
+++ src/components/header/userNavigation/userNavigation.js
@@ -26,7 +26,7 @@ const UserNavigation = (props) => {
       {role === 'ROLE_COACH' && <>
         <Button
           disableElevation
-          className={`${location.pathname === '/verify-tests' ? 'bold' : null} ${props.roleBtns}`}
+          className={`${location.pathname === '/tests-for-verification' ? 'bold' : null} ${props.roleBtns}`}
           component={Link}
           to='/verifytests'>
           <Trans>Tests for verification</Trans>
