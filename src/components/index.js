1575d464f8729392ab0a5ab631f3ce8665a47bcf
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-07-30 16:03:24 +0300

    added file EmployeesFilter

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { EditTestsFilter } from './EditTestsFilter/EditTestsFilter';
 export { EditTestsTable } from './EditTestsTable/EditTestsTable';
 export { TestLevelSelector } from './TestLevelSelector/TestLevelSelector';
 export { EmployeesTable } from './EmployeesTable/EmployeesTable';
+export { EmployeesFilter } from './EmployeesFilter/EmployeesFilter';
--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../components/Layout/Layout';
-import { EmployeesTable } from '../../components';
+import { EmployeesFilter, EmployeesTable } from '../../components';
 
 export const Employees = () => {
 
+  const [userName, setUserName] = useState(null);
+
   return (
     <Layout>
+      <EmployeesFilter userName={userName} setUserName={setUserName}/>
       <EmployeesTable />
     </Layout>
   );
