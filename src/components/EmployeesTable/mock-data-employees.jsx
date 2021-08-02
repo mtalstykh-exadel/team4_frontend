09ea904c1b2def2e1bfa8d5e4ffa4efc85e6faa5
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-08-02 09:36:26 +0300

    added id for mock-data-employees

--- src/components/EmployeesTable/mock-data-employees.jsx
+++ src/components/EmployeesTable/mock-data-employees.jsx
@@ -4,14 +4,16 @@ export const filteredEmloyees = [
     level: 'A2',
     testDeadline: '30 Jul 2021, 10:54',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: true
+    assigne: true,
+    id: 456321
   },
   {
     name: 'Petrov Petr',
     level: 'A1',
     testDeadline: '24 Jul 2021, 10:54',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: true
+    assigne: true,
+    id: 456322
   },
   {
     name: 'Sergeev Sergey',
@@ -19,117 +21,134 @@ export const filteredEmloyees = [
     testDeadline: '24 Jul 2021, 10:54',
     mail: 'yaroslavsmirnov@gmail.com',
     assigne: true,
+    id: 456323
   },
   {
     name: 'Savchenko Mariya',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456324
   },
   {
     name: 'Ivanov Ivan',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456325
   },
   {
     name: 'Petrov Petr',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456326
   },
   {
     name: 'Sergeev Sergey',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456327
   },
   {
     name: 'Savchenko Mariya',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456328
   },
   {
     name: 'Ivanov Ivan',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456329
   },
   {
     name: 'Petrov Petr',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456330
   },
   {
     name: 'Sergeev Sergey',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456331
   },
   {
     name: 'Savchenko Mariya',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456332
   },
   {
     name: 'Ivanov Ivan',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456333
   },
   {
     name: 'Petrov Petr',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456334
   },
   {
     name: 'Sergeev Sergey',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456335
   },
   {
     name: 'Savchenko Mariya',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456336
   },
   {
     name: 'Ivanov Ivan',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456337
   },
   {
     name: 'Petrov Petr',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456338
   },
   {
     name: 'Sergeev Sergey',
     level: '',
     testDeadline: '',
     mail: 'yaroslavsmirnov@gmail.com',
-    assigne: false
+    assigne: false,
+    id: 456339
   }
 ];
