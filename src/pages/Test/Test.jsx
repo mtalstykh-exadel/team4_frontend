121601d3c5fe3e0476afe969debcc1b922911864
Kseniya <pupkoksenia@gmail.com>
2021-08-07 12:58:07 +0300

    CoachTestVerification modal window

--- src/pages/Test/Test.jsx
+++ src/pages/Test/Test.jsx
@@ -13,7 +13,6 @@ import Layout from '../../components/Layout/Layout';
 import Button from '@material-ui/core/Button';
 import './Test.scss';
 import { Trans } from '@lingui/macro';
-import {CoachTestVerification} from '../../components/Test/CoachTestVerification/CoachTestVerification';
 
 export const Test = () => {
   const TestDurationInMinutes = 40;
@@ -52,7 +51,6 @@ export const Test = () => {
 
   return (
     <Layout>
-      <CoachTestVerification/>
       <div className='test-page font-primary'>
         <div className='test-navigation-wrapper'>
           <div
