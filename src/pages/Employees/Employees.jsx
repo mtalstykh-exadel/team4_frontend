70865a1b12f98af93c8772c006571c44b4c53089
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-08-08 22:19:04 +0300

    Add routing protection

--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
 import Layout from '../../components/Layout/Layout';
 import { EmployeesFilter, EmployeesTable } from '../../components';
 import './Employees.scss';
@@ -6,7 +9,9 @@ import './Employees.scss';
 export const Employees = () => {
 
   const [userName, setUserName] = useState(null);
+  const role = useSelector((state) => state.jwt.role);
 
+  if (role !== 'ROLE_HR') return <Redirect to='/' />;
   return (
     <Layout pageWrapperClass='employees-wrapper'>
       <EmployeesFilter setUserName={setUserName}/>
@@ -14,3 +19,25 @@ export const Employees = () => {
     </Layout>
   );
 };
+
+/*
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+
+import { ManageModule } from '../../components';
+import Layout from '../../components/Layout/Layout';
+
+export const ManageTest = () => {
+
+  const role = useSelector((state) => state.auth.isAuth);
+
+  if (role !== 'ROLE_COACH') return <Redirect to='/' />;
+  return (
+    <Layout>
+      <ManageModule />
+    </Layout>
+  );
+};
+
+*/
