a4e5e0ae2135343c53807f86a8eb5dee6a623e77
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-08-08 22:24:05 +0300

    Remove comments

--- src/pages/AdminDistribution/AdminDistribution.jsx
+++ src/pages/AdminDistribution/AdminDistribution.jsx
@@ -181,27 +181,3 @@ AdminDistribution.propTypes = {
 };
 
 export default AdminDistribution;
-
-/*
-
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-
-import { ManageModule } from '../../components';
-import Layout from '../../components/Layout/Layout';
-
-export const ManageTest = () => {
-
-  const role = useSelector((state) => state.jwt.role);
-
-  if (role !== 'ROLE_COACH') return <Redirect to='/' />;
-  return (
-    <Layout>
-      <ManageModule />
-    </Layout>
-  );
-};
-
-
-*/
--- src/pages/Employees/Employees.jsx
+++ src/pages/Employees/Employees.jsx
@@ -19,25 +19,3 @@ export const Employees = () => {
     </Layout>
   );
 };
-
-/*
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
-
-import { ManageModule } from '../../components';
-import Layout from '../../components/Layout/Layout';
-
-export const ManageTest = () => {
-
-  const role = useSelector((state) => state.auth.isAuth);
-
-  if (role !== 'ROLE_COACH') return <Redirect to='/' />;
-  return (
-    <Layout>
-      <ManageModule />
-    </Layout>
-  );
-};
-
-*/
