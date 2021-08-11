c7f7119dedf80f8db82414ef7176bb1ac33ebeb2
petemarianne <petia2001@icloud.com>
2021-08-11 16:48:41 +0300

    translation

--- src/components/TestsForVerificationTable/Component/TestsForVerificationModal.jsx
+++ src/components/TestsForVerificationTable/Component/TestsForVerificationModal.jsx
@@ -6,6 +6,7 @@ import { IconButton, Button, TextField, Paper } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { Player } from '../../index';
 import { Link } from 'react-router-dom';
+import { Trans } from '@lingui/macro';
 
 export const TestsForVerificationModal = ({id, level, handleClose}) => {
   const [essayGrade, setEssayGrade] = useState(-1);
@@ -13,25 +14,25 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
 
   const ReportedMistakes = [
     {
-      module: 'Listening',
+      module: ['Listening','Аудирование'],
       message: 'Hello! This question contains an error. The correct answer is not so-and-so, but so-and-so.',
       questionID: 1258454,
       question: 'An obstetrician/gynecologist at the pre-conception clinic suggests we ............. some further tests.'
     },
     {
-      module: 'Essay',
+      module: ['Essay','Эссе'],
       message: 'Hello! This topic contains an error. It is right to write birTHday.',
       questionID: 1908632,
       question: 'Happy birzday to you!'
     },
     {
-      module: 'Essay',
+      module: ['Essay','Эссе'],
       message: 'Hello! This topic contains an error. It is right to write birTHday.',
       questionID: 1908632,
       question: 'Happy birzday to you!'
     },
     {
-      module: 'Essay',
+      module: ['Essay','Эссе'],
       message: 'Hello! This topic contains an error. It is right to write birTHday.',
       questionID: 1908632,
       question: 'Happy birzday to you!'
@@ -40,15 +41,15 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
 
   const ReportedMistakesHTML =
     <div className='reported-mistake-wrapper'>
-      <div className='error-messages'>Error messages from the user:</div>
+      <div className='error-messages'><Trans>Error messages from the user:</Trans></div>
       <div className='scroll-container'>
         {
           ReportedMistakes.map((item) => {
             return (
               <>
-                <div className='module-name'>Module {item.module}</div>
+                <div className='module-name'><Trans>Module</Trans> <Trans>{item.module[0]}{item.module[1]}</Trans></div>
                 <div className='users-message'>{item.message}</div>
-                <div className='question-id'>Question ID {item.questionID}</div>
+                <div className='question-id'><Trans>Question ID</Trans> {item.questionID}</div>
                 <div className='question-context'>{item.question}</div>
                 <div className='edit-button-wrapper'>
                   <Button
@@ -57,7 +58,7 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
                     className='edit-button'
                     component={Link}
                     to='/edit-test-modules'
-                  >Edit</Button>
+                  ><Trans>Edit</Trans></Button>
                 </div>
                 <TextField
                   label='Comment'
@@ -74,7 +75,7 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
     </div>;
   const EssayHTML =
     <div className='essay-wrapper'>
-      <div className='topic-title'>Topic</div>
+      <div className='topic-title'><Trans>Topic</Trans></div>
       <div className='topic-text'>Essay topic</div>
       <div className='users-essay'>User's essay</div>
       <div className='grades-wrapper'>
@@ -96,7 +97,7 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
     </div>;
   const SpeakingHTML =
     <div className='speaking-wrapper'>
-      <div className='topic-title'>Topic</div>
+      <div className='topic-title'><Trans>Topic</Trans></div>
       <div className='topic-text'>Speaking topic</div>
       <div className='audio'>
         <Player
@@ -133,24 +134,24 @@ export const TestsForVerificationModal = ({id, level, handleClose}) => {
       <Paper elevation={2}>
         <div className='tests-verification-modal-header'>
           <div className='test-information'>
-            <span className='test-id-verification-modal'>test ID {id}</span>
-            <span>Level {level}</span>
+            <span className='test-id-verification-modal'><Trans>Test ID</Trans> {id}</span>
+            <span><Trans>Level</Trans> {level}</span>
           </div>
           <IconButton aria-label='close' onClick={handleClose} className='close-icon-wrapper'>
             <CloseIcon/>
           </IconButton>
         </div>
         <div className='tests-verification-modal-navigation'>
-          <div className={step === 0 ? 'reported-mistake-navigation chosen' : 'reported-mistake-navigation'} onClick={() => {setStep(0);}}><div className='navigation-text'>Reported mistakes</div></div>
-          <div className={step === 1 ? 'essay-navigation chosen' : 'essay-navigation'} onClick={() => {setStep(1);}}><div className='navigation-text'>Essay</div></div>
-          <div className={step === 2 ? 'speaking-navigation chosen' : 'speaking-navigation'} onClick={() => {setStep(2);}}><div className='navigation-text'>Speaking</div></div>
+          <div className={step === 0 ? 'reported-mistake-navigation chosen' : 'reported-mistake-navigation'} onClick={() => {setStep(0);}}><div className='navigation-text'><Trans>Reported mistakes</Trans></div></div>
+          <div className={step === 1 ? 'essay-navigation chosen' : 'essay-navigation'} onClick={() => {setStep(1);}}><div className='navigation-text'><Trans>Essay</Trans></div></div>
+          <div className={step === 2 ? 'speaking-navigation chosen' : 'speaking-navigation'} onClick={() => {setStep(2);}}><div className='navigation-text'><Trans>Speaking</Trans></div></div>
         </div>
         <div className='tests-verification-modal-context'>
           {steps[step]}
         </div>
         <div className='tests-verification-modal-buttons-wrapper'>
-          <Button variant='contained' color='primary' className='save-button'>Save</Button>
-          <Button variant='contained' color='secondary' className='submit-button'>Submit</Button>
+          <Button variant='contained' color='primary' className='save-button'><Trans>Save</Trans></Button>
+          <Button variant='contained' color='secondary' className='submit-button'><Trans>Submit</Trans></Button>
         </div>
       </Paper>
     </div>
