9bdbff785eee089b77412f676a4cf49bc4db9586
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-08-12 19:47:43 +0300

    fix spacing

--- src/api/test-set.js
+++ src/api/test-set.js
@@ -13,12 +13,9 @@ const saveSpeaking = ( blob ) => {
   const formData = new FormData();
   formData.append('file', blob, {type: blob.type});
 
-  return axiosInstance.post('/answer/speaking/' + JSON.parse(localStorage.getItem(currentTest)).id, formData,
-    {
-      headers: { 'Content-Type': 'multipart/form-data;'
-      }
-    }
-  );
+  return axiosInstance.post('/answer/speaking/' + JSON.parse(localStorage.getItem(currentTest)).id, formData, {
+    headers: { 'Content-Type': 'multipart/form-data;'}
+  });
 };
 
 export {saveSpeaking, saveListeningAndGrammar, saveEssay};
