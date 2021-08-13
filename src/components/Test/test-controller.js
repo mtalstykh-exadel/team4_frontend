bc2f80999ba0f4e2606d86c241287974cb56d91b
DmitryqV <vlasenko_dima@icloud.com>
2021-08-13 14:12:57 +0500

    bug fix

--- src/components/Test/test-controller.js
+++ src/components/Test/test-controller.js
@@ -2,18 +2,18 @@ export const testController = ({ tasks, testModule, questionID, answerID, domID 
   tasks.map((el) => {
     if (el.id === questionID) {
       if (localStorage.getItem(testModule) !== null) {
-        const storage = JSON.parse(localStorage.getItem(testModule));
         localStorage.setItem(
           testModule,
           JSON.stringify([
             { qID: questionID, aID: answerID, domID: domID },
-            ...storage
+            ...JSON.parse(localStorage.getItem(testModule))
           ])
         );
 
-        const uniqueArray = storage.filter((current, index) => {
-          return index === storage.findIndex((obj) => {
-            if (JSON.stringify(obj.qID) === JSON.stringify(current.qID)) {
+        const uniqueArray = JSON.parse(localStorage.getItem(testModule)).filter((el, index) => {
+          const currentElement = JSON.stringify(el.qID);
+          return index === JSON.parse(localStorage.getItem(testModule)).findIndex((obj) => {
+            if (JSON.stringify(obj.qID) === currentElement) {
               return [{qID: questionID, aID: answerID, domID: domID}];
             }
           });
