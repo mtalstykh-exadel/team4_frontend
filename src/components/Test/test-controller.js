eac2dfc8bd1890ea7ded94469182fd622e911b61
DmitryqV <vlasenko_dima@icloud.com>
2021-08-13 14:05:38 +0500

    now only unique answers are stored in the storage of tests, and not their entire list

--- src/components/Test/test-controller.js
+++ src/components/Test/test-controller.js
@@ -2,13 +2,25 @@ export const testController = ({ tasks, testModule, questionID, answerID, domID 
   tasks.map((el) => {
     if (el.id === questionID) {
       if (localStorage.getItem(testModule) !== null) {
+        const storage = JSON.parse(localStorage.getItem(testModule));
         localStorage.setItem(
           testModule,
           JSON.stringify([
-            ...JSON.parse(localStorage.getItem(testModule)),
             { qID: questionID, aID: answerID, domID: domID },
+            ...storage
           ])
         );
+
+        const uniqueArray = storage.filter((current, index) => {
+          return index === storage.findIndex((obj) => {
+            if (JSON.stringify(obj.qID) === JSON.stringify(current.qID)) {
+              return [{qID: questionID, aID: answerID, domID: domID}];
+            }
+          });
+        });
+
+        localStorage.setItem(testModule, JSON.stringify([ ...uniqueArray ]));
+
       } else {
         localStorage.setItem(
           testModule,
