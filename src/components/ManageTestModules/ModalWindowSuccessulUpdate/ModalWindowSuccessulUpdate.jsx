6c92794213662389dbfdf3e6b62062e7fc8df923
Alesia Drazdova <alesiadrozdova@gmail.com>
2021-08-13 18:15:58 +0300

    create modal window

---
+++ src/components/ManageTestModules/ModalWindowSuccessulUpdate/ModalWindowSuccessulUpdate.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+
+import { IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+import './ModalWindowSuccessulUpdate.scss';
+
+export const ModalWindowSuccessulUpdate = ({ handleClose }) => {
+
+  return (
+    <div className='modal-remove-post'>
+      <IconButton aria-label='close' onClick={handleClose} className='close-icon-wrapper'>
+        <CloseIcon className='close-icon' />
+      </IconButton>
+      <div className='text'>The data was saved successfully</div>
+      <div className='btn'>
+        <Button variant='outlined' color='primary' onClick={handleClose}>Close</Button>
+      </div>
+    </div>
+  );
+};
+
+ModalWindowSuccessulUpdate.propTypes = {
+  handleClose: PropTypes.func,
+};
--- src/components/TestsForVerificationTable/TestsForVerificationTable.jsx
+++ src/components/TestsForVerificationTable/TestsForVerificationTable.jsx
@@ -9,22 +9,22 @@ export const TestsForVerificationTable = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [open, setOpen] = useState(false);
-  const [levelAndID, setLevelAndID] = useState(['id','level']);
+  const [levelAndID, setLevelAndID] = useState(['id', 'level']);
 
-  const rows = [['ID','ID'], ['Level','Уровень'], ['Assigned','Дата назначения'], ['Test deadline', 'Крайний срок сдачи'], ['Priority', 'Приоритет'], ['Action','Действие']];
+  const rows = [['ID', 'ID'], ['Level', 'Уровень'], ['Assigned', 'Дата назначения'], ['Test deadline', 'Крайний срок сдачи'], ['Priority', 'Приоритет'], ['Action', 'Действие']];
   const testForVerification = [
-    { id: 765987, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium','средний'] },
-    { id: 765988, level: 'A2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
-    { id: 765989, level: 'B1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
-    { id: 765990, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium','средний'] },
+    { id: 765987, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium', 'средний'] },
+    { id: 765988, level: 'A2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
+    { id: 765989, level: 'B1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
+    { id: 765990, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium', 'средний'] },
     { id: 765991, level: 'C1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['low', 'низкий'] },
-    { id: 765992, level: 'B2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
-    { id: 765993, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium','средний'] },
-    { id: 765994, level: 'A2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
-    { id: 765995, level: 'B1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
-    { id: 765996, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium','средний'] },
+    { id: 765992, level: 'B2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
+    { id: 765993, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium', 'средний'] },
+    { id: 765994, level: 'A2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
+    { id: 765995, level: 'B1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
+    { id: 765996, level: 'A1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['medium', 'средний'] },
     { id: 765997, level: 'C1', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['low', 'низкий'] },
-    { id: 765998, level: 'B2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high','высокий'] },
+    { id: 765998, level: 'B2', assignedDate: '19 Jun 2021, 10:54', testDeadlineDate: '30 Jun 2021, 10:54', priority: ['high', 'высокий'] },
   ];
 
   const handleChangePage = (event, newPage) => {
@@ -70,13 +70,13 @@ export const TestsForVerificationTable = () => {
                   <TableCell align='left'><Trans>{row.priority[0]}{row.priority[1]}</Trans></TableCell>
                   <TableCell align='left'>
                     <Button color='primary'
-                            variant='outlined'
-                            size='small'
-                            style={{ width: 110 }}
-                            onClick={() => {
-                              setLevelAndID([row.id,row.level]);
-                              handleOpen();
-                            }}
+                      variant='outlined'
+                      size='small'
+                      style={{ width: 110 }}
+                      onClick={() => {
+                        setLevelAndID([row.id, row.level]);
+                        handleOpen();
+                      }}
                     >
                       <Trans>Verify</Trans>
                     </Button>
@@ -104,7 +104,7 @@ export const TestsForVerificationTable = () => {
           aria-describedby='simple-modal-description'
           className='modal'>
           <div className='modal-content'>
-            <TestsForVerificationModal id={levelAndID[0]} level={levelAndID[1]} handleClose={handleClose}/>
+            <TestsForVerificationModal id={levelAndID[0]} level={levelAndID[1]} handleClose={handleClose} />
           </div>
         </Modal>
       </Paper>
