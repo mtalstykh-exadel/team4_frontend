3fda58ef0f596a1d676c26614d76a22ce815fa11
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-08-15 21:57:21 +0300

    change unverified tests actions

--- src/store/actions/unverifiedTestActions.js
+++ src/store/actions/unverifiedTestActions.js
@@ -1,18 +1,16 @@
 import { REQUEST_UNVERIFIED_TESTS, REQUEST_REPORTS } from './actionTypes';
-import getUnverifiedTests from '../../api/unverifiedTests-fetech';
-
-import getReports from '../../api/reports-fetch';
+import { getUnverifiedTests, getReports } from '../../api/unverifiedTests-fetch';
 
 export const setUnverifiedTests = (filteredEmployees) => ({ type: REQUEST_UNVERIFIED_TESTS, filteredEmployees });
 
 export const setReports = (filteredEmployees) => ({ type: REQUEST_REPORTS, filteredEmployees });
 
 export const requestUnverifiedTests = () => (dispatch) => {
-  getUnverifiedTests()
+  return getUnverifiedTests()
     .then((data) => (dispatch(setUnverifiedTests(data))));
 };
 
 export const requestReports = (testId) => (dispatch) => {
-  getReports(testId)
+  return getReports(testId)
     .then((data) => (dispatch(setReports(data))));
 };
