85351a33402d017424a3dfcf55df704a7c0b637e
Feoktist Shovchko <shovchko.fbusiness@gmail.com>
2021-08-17 22:32:10 +0300

    fix profile buttons

--- src/components/TestsInfo/TestsData/TestsData.jsx
+++ src/components/TestsInfo/TestsData/TestsData.jsx
@@ -38,6 +38,10 @@ const TestsData = (props) => {
     else null;
   };
 
+  const getResult = (testId) => {
+    localStorage.setItem(currentTest, JSON.stringify({id: testId}));
+    history.push('/result');
+  };
 
   const filterRow = (row) => {
     const filteredRow = [];
@@ -116,30 +120,46 @@ const TestsData = (props) => {
                                 ?
                                 <CircularProgress className='border-primary' size='25px' />
                                 :
-                                <Button className='button-standard' color='primary' variant='contained'
-                                  onClick={() => {
-                                    setLoading(true);
-                                    localStorage.removeItem(currentTest);
-                                    localStorage.removeItem(testGrammarUserAnswers);
-                                    localStorage.removeItem(testEassyUserAnswers);
-                                    localStorage.removeItem(testListeningUserAnswers);
-                                    localStorage.removeItem(testSpeakingAnswers);
-                                    startTestById(row.testId)
-                                      .then((response) => {
-                                        localStorage.setItem(currentTest, JSON.stringify(response));
+                                row.status === 'ASSIGNED'
+                                  ?
+                                  <Button className='button-standard' color='primary' variant='contained'
+                                    onClick={() => {
+                                      setLoading(true);
+                                      localStorage.removeItem(currentTest);
+                                      localStorage.removeItem(testGrammarUserAnswers);
+                                      localStorage.removeItem(testEassyUserAnswers);
+                                      localStorage.removeItem(testListeningUserAnswers);
+                                      localStorage.removeItem(testSpeakingAnswers);
+                                      startTestById(row.testId)
+                                        .then((response) => {
+                                          localStorage.setItem(currentTest, JSON.stringify(response));
+                                          history.push('/test');
+                                          window.scrollTo(0, 0);
+                                        })
+                                        .catch((err) => {
+                                          setLoading(false);
+                                          if (err.code === 409) {
+                                            handleOpen();
+                                          }
+                                        });
+                                    }
+                                    } >
+                                    <Trans>Take Test</Trans>
+                                  </Button>
+                                  :
+                                  row.status === 'VERIFIED' ? <Button className='button-standard' color='primary' variant='contained'
+                                    onClick={() => {
+                                      getResult(row.testId);
+                                    }} >
+                                    <Trans>View Results</Trans>
+                                  </Button>
+                                    :
+                                    <Button className='button-standard' color='primary' variant='contained'
+                                      onClick={() => {
                                         history.push('/test');
-                                        window.scrollTo(0, 0);
-                                      })
-                                      .catch((err) => {
-                                        setLoading(false);
-                                        if (err.code === 409) {
-                                          handleOpen();
-                                        }
-                                      });
-                                  }
-                                  } >
-                                  <Trans>Take Test</Trans>
-                                </Button>
+                                      }} >
+                                      <Trans>Continue</Trans>
+                                    </Button>
                               : Array.isArray(value) ? <Trans>{value[0]}{value[1]}</Trans> : value
                           }
                         </TableCell>
