d675f9680fc5f5c89af0bee1bd87b26009d59870
ИТ-201 <egornesvickij@gmail.com>
2021-08-17 10:57:44 +0500

    delete /

--- src/pages/AdminDistribution/ScriptsAdminDistributtion.js
+++ src/pages/AdminDistribution/ScriptsAdminDistributtion.js
@@ -1,6 +1,6 @@
 const assignTest = (id) => {
   const buttonAssign = document.getElementById('item-' + id + '-button');
-  const selectNames = document.getElementById('item-' + id + '-select');\
+  const selectNames = document.getElementById('item-' + id + '-select');
   
   if (selectNames.value !== 'placeholder') {
     buttonAssign.textContent = 'DEASSIGN';
