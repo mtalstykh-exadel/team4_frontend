97e0e5225bc29d661ea898a305103554421b3ce9
DmitryqV <vlasenko_dima@icloud.com>
2021-08-18 17:12:25 +0500

    display of attempts counter

--- src/components/Test/Listening/Listening.jsx
+++ src/components/Test/Listening/Listening.jsx
@@ -4,6 +4,7 @@ import './Listening.scss';
 import { Grammar, Player } from '../../index';
 import { Trans } from '@lingui/macro';
 import { getAudioFile } from '../../../api/get-audioFIle';
+import { testAudioAttempts } from '../../../constants/localStorageConstants';
 
 export const Listening = ({ tasks, contentFile, testModule, reportModule, level, testID, module }) => {
   const [url, setUrl] = useState('');
@@ -28,6 +29,9 @@ export const Listening = ({ tasks, contentFile, testModule, reportModule, level,
       </div>
       <div className='audio'>
         <Player id='player-listening' src={url} />
+        <p className='step-description'> 
+          you have {localStorage.getItem(testAudioAttempts)} attempts left to listen to audio 
+        </p>
       </div>
       <Grammar tasks={tasks} testModule={testModule} level={level} testID={testID} reportModule={reportModule} module={module}/>
     </div>
