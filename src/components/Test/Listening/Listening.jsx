f0f5c8e5a447b154a4315bd28cf3afa3c1139054
DmitryqV <vlasenko_dima@icloud.com>
2021-08-19 13:26:11 +0500

    fix rewied

--- src/components/Test/Listening/Listening.jsx
+++ src/components/Test/Listening/Listening.jsx
@@ -29,8 +29,8 @@ export const Listening = ({ tasks, contentFile, testModule, reportModule, level,
       </div>
       <div className='audio'>
         <Player id='player-listening' src={url} />
-        <p className='step-description'> 
-          You have {localStorage.getItem(testAudioAttempts)} attempts left to listen to audio 
+        <p className='step-description'>
+          You have {localStorage.getItem(testAudioAttempts)} attempts left to listen to audio
         </p>
       </div>
       <Grammar tasks={tasks} testModule={testModule} level={level} testID={testID} reportModule={reportModule} module={module}/>
